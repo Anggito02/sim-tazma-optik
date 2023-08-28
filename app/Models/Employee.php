5444dea14dfb71173b65cc5f83e0c4f7087b69d0
Anggito02 <anggito_anju@yahoo.com>
2023-08-28 22:42:11 +0700

    [FEAT] add employee migration and employee model [STYLE] change serviceprovider to service

---
+++ app/Models/Employee.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'username',
+        'nik',
+        'employee_name',
+        'department',
+        'section',
+        'position',
+        'role',
+        'plant',
+        'status',
+    ];
+}
---
+++ database/migrations/2023_08_28_152606_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('nik')->unique();
+            $table->text('employee_name');
+            $table->string('department');
+            $table->string('section');
+            $table->string('position');
+            $table->string('role')->default('user');
+            $table->string('plant');
+            $table->enum('status', ['active', 'inactive']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
