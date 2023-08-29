88d5fbf93c9fdc85585fc54c7850bbc3886bb24e
abhiramadamas <muhammaddamas81@gmail.com>
2023-08-29 10:06:59 +0700

    [FEAT] Added user UI

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -68,6 +68,7 @@
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         {{-- <h6 class="collapse-header">Custom Warna:</h6> --}}
+                        <a class="collapse-item" href="{{url('dashboard/admin/user/create')}}">User</a>
                         <a class="collapse-item" href="{{url('dashboard/admin/warna/create')}}">Warna</a>
                         <a class="collapse-item" href="{{url('dashboard/admin/warna')}}">Vendor Information</a>
                     </div>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,219 @@
+@extends('layout')
+@section('content')
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    {{-- <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+    <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+        For more information about DataTables, please visit the <a target="_blank"
+            href="https://datatables.net">official DataTables documentation</a>.</p> --}}
+
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">USER</h6>
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn-sm btn-success float-right" data-toggle="modal"
+                data-target="#exampleModalCenter">
+                Add New
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">New Data</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="post" action="">
+                                @csrf
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <th>Username</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>NIK</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Employee Name</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    <tr>
+                                    <tr>
+                                        <th>Department</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    <tr>
+                                    <tr>
+                                        <th>Section</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Position</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>User Role</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+                                    <tr>
+                                        <th>Plant</th>
+                                        <td><input type="text" name="title" class="form-control" /></td>
+                                    </tr>
+
+                                        <td colspan="2">
+                                            <input type="submit" class="btn btn-success" />
+                                        </td>
+                                    </tr>
+                                </table>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            {{-- <button type="button" class="btn btn-success">Save</button> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Username</th>
+                            <th>NIK</th>
+                            <th>Employee Name</th>
+                            <th>Department</th>
+                            <th>Section</th>
+                            <th>Position</th>
+                            <th>User Role</th>
+                            <th>Plant</th>
+                            <th>Detail</th>
+                            <th>Delete</th>
+                            <th>Status Active</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>No</th>
+                            <th>Username</th>
+                            <th>NIK</th>
+                            <th>Employee Name</th>
+                            <th>Department</th>
+                            <th>Section</th>
+                            <th>Position</th>
+                            <th>User Role</th>
+                            <th>Plant</th>
+                            <th>Detail</th>
+                            <th>Delete</th>
+                            <th>Status Active</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Teddy123</td>
+                            <td>123456789</td>
+                            <td>Teddy</td>
+                            <td>SALES</td>
+                            <td>SALES & PURCHASED</td>
+                            <td>SALES HEAD</td>
+                            <td>Administrator</td>
+                            <td>KPS-IA|CENTRAL</td>
+                            <td>
+                                <!-- Button trigger modal Edit -->
+                                <button type="button" class="btn-sm btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#staticBackdrop">
+                                    <i class="fa fa-edit"></i>
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static"
+                                    data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Data</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <form method="post">
+                                                    <table class="table table-bordered">
+                                                        <tr>
+                                                            <th>Warna</th>
+                                                            <td><input type="text" name="warna" class="form-control">
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+
+                                                </form>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-success">Update</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <!-- Button trigger modal Delete -->
+                                <button type="button" class="btn-sm btn-danger" data-toggle="modal"
+                                    data-target="#exampleModalCenterDelete">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="exampleModalCenterDelete" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLongTitle">Delete Data</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Are you sure you want to delete?</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">No</button>
+                                                <button type="button" class="btn btn-danger">Yes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Active
+                            </td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+
+
+@endsection
