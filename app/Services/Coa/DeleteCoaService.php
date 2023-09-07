044453694ac6124f5963ad9286e76e1210e0cf6c
Anggito02 <anggito_anju@yahoo.com>
2023-09-07 18:09:26 +0700

    [STYLE] add validation to coa master module

--- app/Services/Coa/DeleteCoaService.php
+++ app/Services/Coa/DeleteCoaService.php
@@ -23,7 +23,7 @@ class DeleteCoaService {
         try {
             // Validate request
             $request->validate([
-                'id' => 'required',
+                'id' => 'required|exists:coas,id',
             ]);
 
             $id = $request->id;
