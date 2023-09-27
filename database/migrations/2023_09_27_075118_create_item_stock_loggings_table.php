b7afc50a4575727e217ccf458ee0cd2b80ba0ce2
Anggito02 <anggito_anju@yahoo.com>
2023-09-27 16:14:21 +0700

    [FEAT] add item stock update log

---
+++ database/migrations/2023_09_27_075118_create_item_stock_loggings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item_stock_loggings', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('tanggal_berubah');
+            $table->integer('stok_lama');
+            $table->integer('stok_baru');
+            $table->enum('bentuk_perubahan', ['penambahan', 'pengurangan']);
+
+            // Foreign Keys
+            // Item
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item_stock_loggings');
+    }
+};
