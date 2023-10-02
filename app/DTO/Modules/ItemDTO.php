81705c173e89ed39a359a8d9547208d1b6679c03
Anggito02 <anggito_anju@yahoo.com>
2023-10-02 21:47:45 +0700

    [TESTING] get all item

--- app/DTO/Modules/ItemDTO.php
+++ app/DTO/Modules/ItemDTO.php
@@ -9,8 +9,8 @@ class ItemDTO {
         public ?string $kode_item,
         public ?string $deskripsi,
         public int $stok,
-        public int $harga_beli,
-        public int $harga_jual,
+        public string $harga_beli,
+        public string $harga_jual,
 
         // Frame
         public ?string $frame_sku_vendor,
