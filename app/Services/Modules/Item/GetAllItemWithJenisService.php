4b9d548bd9dddc37bc3ea1787520de6b480c1d84
Anggito02 <anggito_anju@yahoo.com>
2023-10-02 21:31:43 +0700

    [TESTING] get all item

--- app/Services/Modules/Item/GetAllItemWithJenisService.php
+++ app/Services/Modules/Item/GetAllItemWithJenisService.php
@@ -31,7 +31,7 @@ class GetAllItemWithJenisService {
             return response()->json([
                 'status' => 'success',
                 'message' => 'Check request',
-                'data' => $request->all()
+                'data' => $request->jenis_item, $request->page, $request->limit
             ], 200);
 
             $itemDTO = $this->itemRepository->getAllItem($request->jenis_item, $request->page, $request->limit);
