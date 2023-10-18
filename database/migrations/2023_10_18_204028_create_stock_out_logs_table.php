9996d13b8b22e5b75a9183b3634a7528a92c27f9
Anggito02 <anggito_anju@yahoo.com>
2023-10-18 20:42:03 +0700

    [FEAT] add stock log table [FIX] stock in procedure name

---
+++ database/migrations/2023_10_18_204028_create_stock_out_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_out_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_item');
+            $table->integer('bulan');
+            $table->integer('tahun');
+            $table->integer('stok_total');
+            $table->integer('stok_out_total');
+            $table->timestamps();
+
+            // Foreign Key
+            // Item
+            $table->foreignId('item_id')->constrained('items')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_out_logs');
+    }
+};
