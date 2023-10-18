8f14611fe6b48bb8640c9ff60f39c29ea5530322
Anggito02 <anggito_anju@yahoo.com>
2023-10-19 01:33:31 +0700

    [FIX] fix get receive order repo

--- app/Repositories/Modules/ReceiveOrder/GetReceiveOrderWithInfoRepository.php
+++ app/Repositories/Modules/ReceiveOrder/GetReceiveOrderWithInfoRepository.php
@@ -17,6 +17,21 @@ class GetReceiveOrderWithInfoRepository {
                 ->select('receive_orders.*', 'received_by.employee_name as received_by_name', 'checked_by.employee_name as checked_by_name', 'approved_by.employee_name as approved_by_name')
                 ->where('receive_orders.purchase_order_id', '=', $po_id)
                 ->first();
+
+            $receiveOrderDTO = [
+                'id' => $receive_order->id,
+                'nomor_receive_order' => $receive_order->nomor_receive_order,
+                'tanggal_penerimaan' => $receive_order->tanggal_penerimaan,
+                'purchase_order_id' => $receive_order->purchase_order_id,
+                'received_by' => $receive_order->received_by,
+                'received_by_name' => $receive_order->received_by_name,
+                'checked_by' => $receive_order->checked_by,
+                'checked_by_name' => $receive_order->checked_by_name,
+                'approved_by' => $receive_order->approved_by,
+                'approved_by_name' => $receive_order->approved_by_name,
+            ];
+
+            return $receiveOrderDTO;
         } catch (Exception $error) {
             throw new Exception($error->getMessage());
         }
