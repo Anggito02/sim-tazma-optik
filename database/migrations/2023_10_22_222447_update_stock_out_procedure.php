fb8796a128cc512b0b1e3ec8f3ff57d092a260eb
Anggito02 <anggito_anju@yahoo.com>
2023-10-22 22:27:16 +0700

    [FEAT] update stock out procedure

---
+++ database/migrations/2023_10_22_222447_update_stock_out_procedure.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('
+           CREATE PROCEDURE IF NOT EXISTS update_stock_in_procedure(
+               IN kode_item VARCHAR(255),
+               IN bulan INT,
+               IN tahun INT,
+               IN stok_out INT,
+               IN item_id BIGINT
+           )
+           BEGIN
+               UPDATE stock_out_logs
+               SET
+                   stok_total = stok_total + stok_out,
+                   stok_out_total = stok_out
+               WHERE
+                kode_item = kode_item AND
+                bulan = bulan AND
+                tahun = tahun AND
+                item_id = item_id;
+           END;
+       ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP PROCEDURE IF EXISTS `update_stock_in_procedure`');
+    }
+};
