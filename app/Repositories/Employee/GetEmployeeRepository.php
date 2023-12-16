819ceec87123bc6b62aee160bc354a6f5c28c9c7
Anggito02 <anggito_anju@yahoo.com>
2023-12-16 12:04:14 +0700

    [TEST] get employee repo

--- app/Repositories/Employee/GetEmployeeRepository.php
+++ app/Repositories/Employee/GetEmployeeRepository.php
@@ -7,70 +7,70 @@ use Exception;
 use App\DTO\UserDTOs\UserInfoDTO;
 use App\Models\User;
 
-class GetEmployeeRepository {
-    /**
-     * Get employee by id
-     * @param int $id
-     * @return UserInfoDTO
-     */
-    public function getEmployee(int $id) {
-        try {
-            $employee = User::leftJoin('branches', 'users.branch_id', '=', 'branches.id')
-                ->where('users.id', $id)
-                ->select(
-                    'users.id',
-                    'users.email',
-                    'users.username',
-                    'users.nik',
-                    'users.nip',
-                    'users.employee_name',
-                    'users.photo',
-                    'users.gender',
-                    'users.address',
-                    'users.phone',
-                    'users.department',
-                    'users.section',
-                    'users.position',
-                    'users.role',
-                    'users.status',
-                    'users.group',
-                    'users.domicile',
-                    'users.branch_id',
-                    'branches.nama_branch'
-                )
-                ->first();
+// class GetEmployeeRepository {
+//     /**
+//      * Get employee by id
+//      * @param int $id
+//      * @return UserInfoDTO
+//      */
+//     public function getEmployee(int $id) {
+//         try {
+//             $employee = User::leftJoin('branches', 'users.branch_id', '=', 'branches.id')
+//                 ->where('users.id', $id)
+//                 ->select(
+//                     'users.id',
+//                     'users.email',
+//                     'users.username',
+//                     'users.nik',
+//                     'users.nip',
+//                     'users.employee_name',
+//                     'users.photo',
+//                     'users.gender',
+//                     'users.address',
+//                     'users.phone',
+//                     'users.department',
+//                     'users.section',
+//                     'users.position',
+//                     'users.role',
+//                     'users.status',
+//                     'users.group',
+//                     'users.domicile',
+//                     'users.branch_id',
+//                     'branches.nama_branch'
+//                 )
+//                 ->first();
 
-            if (!$employee) {
-                throw new Exception('Employee not found');
-            }
+//             if (!$employee) {
+//                 throw new Exception('Employee not found');
+//             }
 
-            $userDTO = new UserInfoDTO(
-                $employee->id,
-                $employee->email,
-                $employee->username,
-                $employee->nik,
-                $employee->nip,
-                $employee->employee_name,
-                $employee->photo,
-                $employee->gender,
-                $employee->address,
-                $employee->phone,
-                $employee->department,
-                $employee->section,
-                $employee->position,
-                $employee->role,
-                $employee->status,
-                $employee->group,
-                $employee->domicile,
-                $employee->branch_id,
-                $employee->nama_branch
-            );
+//             $userDTO = new UserInfoDTO(
+//                 $employee->id,
+//                 $employee->email,
+//                 $employee->username,
+//                 $employee->nik,
+//                 $employee->nip,
+//                 $employee->employee_name,
+//                 $employee->photo,
+//                 $employee->gender,
+//                 $employee->address,
+//                 $employee->phone,
+//                 $employee->department,
+//                 $employee->section,
+//                 $employee->position,
+//                 $employee->role,
+//                 $employee->status,
+//                 $employee->group,
+//                 $employee->domicile,
+//                 $employee->branch_id,
+//                 $employee->nama_branch
+//             );
 
-            return $userDTO;
-        } catch (Exception $error) {
-            throw new Exception($error->getMessage());
-        }
-    }
-}
+//             return $userDTO;
+//         } catch (Exception $error) {
+//             throw new Exception($error->getMessage());
+//         }
+//     }
+// }
 
 ?>
