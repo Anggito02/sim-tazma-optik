fb3e05766f216e79e8d35f1e51e07a95c0cbdcd7
Anggito02 <anggito_anju@yahoo.com>
2024-03-16 21:42:25 +0700

    fix get all branch item bug

--- app/Repositories/Modules/BranchItem/GetAllBranchItemRepository.php
+++ app/Repositories/Modules/BranchItem/GetAllBranchItemRepository.php
@@ -30,7 +30,7 @@ class GetAllBranchItemRepository {
             }
 
             // use pagination
-            $branchItems = BranchItem::whereRaw($activeFilter)
+            $branchItems = BranchItem::whereRaw($activeFilter ? "($activeFilter)" : null)
                 ->join('items', 'branch_items.item_id', '=', 'items.id')
                 ->join('branches', 'branch_items.branch_id', '=', 'branches.id')->select('branch_items.*', 'items.jenis_item as jenis_item', 'items.kode_item as kode_item', 'items.stok as stok_global', 'branches.kode_branch as kode_branch', 'branches.nama_branch as nama_branch')->paginate($branchItemFilter->getLimit(), ['*'], 'page', $branchItemFilter->getPage());
 
